2878ea0f5e6bacc1e33c7ec929706abf9e68a4ff
John van Zantvoort <john@vanzantvoort.org>
2025-10-30 14:17:00 +0100

    feat: update

--- cmd/bundle/create.go
+++ cmd/bundle/create.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/jvzantvoort/bundle/messages"
+	"github.com/jvzantvoort/bundle/bundle"
 	"github.com/spf13/cobra"
 	log "github.com/sirupsen/logrus"
 )
@@ -38,5 +39,36 @@ func handleCreateCmd(cmd *cobra.Command, args []string) {
 			log.Error(err)
 		}
 		os.Exit(1)
+	}
+
+	path := args[0]
+	title := GetString(*cmd, "title")
+
+	b, err := bundle.Create(path, title)
+	if err != nil {
+		// Distinguish common user errors vs system errors where possible
+		if os.IsNotExist(err) {
+			log.Errorf("directory does not exist: %s", path)
+			os.Exit(1)
+		}
+		// lock.AcquireLock returns an error string for lock contention; treat other errors as system errors
+		log.Errorf("System error: %v", err)
+		os.Exit(2)
+	}
+
+	// Print a human-readable summary similar to the CLI contract
+	log.Info("Bundle Created")
+	log.Info("--------------")
+	log.Infof("Path:     %s", b.Path)
+	if b.Metadata != nil {
+		log.Infof("Checksum: %s", b.Metadata.BundleChecksum)
+		log.Infof("Title:    %s", b.Metadata.Title)
+		log.Infof("Created:  %s", b.Metadata.CreatedAt.Format("2006-01-02 15:04:05"))
+	}
+	if b.Files != nil {
+		log.Infof("Files:    %d", len(b.Files.Records))
+	}
+	if b.State != nil {
+		log.Infof("Size:     %d bytes", b.State.SizeBytes)
 	}
 }
--- cmd/bundle/tag.go
+++ cmd/bundle/tag.go
@@ -4,9 +4,11 @@ Copyright © 2025 John van Zantvoort <john@vanzantvoort.org>
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/jvzantvoort/bundle/messages"
+	"github.com/jvzantvoort/bundle/tag"
 	"github.com/spf13/cobra"
 	log "github.com/sirupsen/logrus"
 )
@@ -23,6 +25,11 @@ func init() {
 	rootCmd.AddCommand(TagCmd)
 	TagCmd.Flags().StringP("tag", "T", "", "mark every line with this tag")
 	TagCmd.Flags().StringP("title", "t", "", "log the contents of this file")
+
+	// Subcommands: add, remove, list
+	TagCmd.AddCommand(tagAddCmd)
+	TagCmd.AddCommand(tagRemoveCmd)
+	TagCmd.AddCommand(tagListCmd)
 }
 
 func handleTagCmd(cmd *cobra.Command, args []string) {
@@ -38,5 +45,128 @@ func handleTagCmd(cmd *cobra.Command, args []string) {
 			log.Error(err)
 		}
 		os.Exit(1)
+	}
+}
+
+// tag add
+var tagAddCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Add tags to a bundle",
+	Run:   handleTagAddCmd,
+}
+
+func handleTagAddCmd(cmd *cobra.Command, args []string) {
+	if verbose {
+		log.SetLevel(log.DebugLevel)
+	}
+	log.Debugf("%s: start", cmd.Use)
+	defer log.Debugf("%s: end", cmd.Use)
+
+	if len(args) < 2 {
+		log.Error("Usage: bundle tag add <path> <tag> [<tag>...]")
+		if err := cmd.Help(); err != nil {
+			log.Error(err)
+		}
+		os.Exit(1)
+	}
+
+	path := args[0]
+	tags := args[1:]
+
+	t, err := tag.Load(path)
+	if err != nil {
+		log.Errorf("System error: %v", err)
+		os.Exit(2)
+	}
+
+	t.Add(tags...)
+	if err := t.Save(path); err != nil {
+		log.Errorf("System error: %v", err)
+		os.Exit(2)
+	}
+
+	log.Info("Tags Added")
+	// Print tags
+	for _, v := range t.List() {
+		fmt.Println(v)
+	}
+}
+
+// tag remove
+var tagRemoveCmd = &cobra.Command{
+	Use:   "remove",
+	Short: "Remove tags from a bundle",
+	Run:   handleTagRemoveCmd,
+}
+
+func handleTagRemoveCmd(cmd *cobra.Command, args []string) {
+	if verbose {
+		log.SetLevel(log.DebugLevel)
+	}
+	log.Debugf("%s: start", cmd.Use)
+	defer log.Debugf("%s: end", cmd.Use)
+
+	if len(args) < 2 {
+		log.Error("Usage: bundle tag remove <path> <tag> [<tag>...]")
+		if err := cmd.Help(); err != nil {
+			log.Error(err)
+		}
+		os.Exit(1)
+	}
+
+	path := args[0]
+	tags := args[1:]
+
+	t, err := tag.Load(path)
+	if err != nil {
+		log.Errorf("System error: %v", err)
+		os.Exit(2)
+	}
+
+	t.Remove(tags...)
+	if err := t.Save(path); err != nil {
+		log.Errorf("System error: %v", err)
+		os.Exit(2)
+	}
+
+	log.Info("Tags Removed")
+}
+
+// tag list
+var tagListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List tags for a bundle",
+	Run:   handleTagListCmd,
+}
+
+func handleTagListCmd(cmd *cobra.Command, args []string) {
+	if verbose {
+		log.SetLevel(log.DebugLevel)
+	}
+	log.Debugf("%s: start", cmd.Use)
+	defer log.Debugf("%s: end", cmd.Use)
+
+	if len(args) != 1 {
+		log.Error("Usage: bundle tag list <path>")
+		if err := cmd.Help(); err != nil {
+			log.Error(err)
+		}
+		os.Exit(1)
+	}
+
+	path := args[0]
+	t, err := tag.Load(path)
+	if err != nil {
+		log.Errorf("System error: %v", err)
+		os.Exit(2)
+	}
+
+	if len(t.Tags) == 0 {
+		log.Info("No tags")
+		return
+	}
+
+	for _, v := range t.List() {
+		fmt.Println(v)
 	}
 }
